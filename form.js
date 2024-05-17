c51498a454191a4a400b9dfd730ba8ab25a5a30b
coskunyarenn <yarenok58@gmail.com>
2024-05-17 15:55:02 -0700

    phoneNumber

--- form.js
+++ form.js
@@ -1,14 +1,46 @@
 // use https://dashboard.emailjs.com/ sign in , get Public_KEY , Service_ID and Template_ID
+document.addEventListener("DOMContentLoaded", () => {
+  const phoneInput = document.getElementById("phone")
+  const phoneError = document.getElementById("phoneError")
 
+  phoneInput.addEventListener("input", (event) => {
+    let value = phoneInput.value
+
+    // Remove non-numeric characters
+    value = value.replace(/\D/g, "")
+
+    // Format the number as (123) 456-7890
+    if (value.length > 3 && value.length <= 6) {
+      value = `(${value.slice(0, 3)}) ${value.slice(3)}`
+    } else if (value.length > 6) {
+      value = `(${value.slice(0, 3)}) ${value.slice(3, 6)}-${value.slice(
+        6,
+        10
+      )}`
+    } else if (value.length > 3) {
+      value = `(${value.slice(0, 3)}) ${value.slice(3)}`
+    }
+
+    phoneInput.value = value
+
+    // Basic validation for phone number length
+    if (value.length >= 10) {
+      phoneError.style.visibility = "hidden"
+    } else {
+      phoneError.style.visibility = "visible"
+      phoneError.textContent = "Phone number must be 10 digits long"
+    }
+  })
+})
 ;(function () {
-  emailjs.init("Public_Key") // Replace with your EmailJS user ID
+  emailjs.init("fUkwVIFzf3fn72ban") // Replace with your EmailJS user ID
 })()
 
 document.getElementById("myForm").addEventListener("submit", function (event) {
   event.preventDefault() // Prevent the default form submission
 
   // Send form data using EmailJS
-  emailjs.sendForm("service_id", "template_id", this).then(
+  emailjs.sendForm("service_z6nddvq", "template_j3luvhh", this).then(
     function (response) {
       console.log("SUCCESS!", response.status, response.text)
 
